84ef2b82dab8f9c7cf87a060a31f465b929570e8
tech-zack <difference19@icloud.com>
2021-02-18 12:38:01 +0900

    ユーザー管理機能訂正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,18 @@ class User < ApplicationRecord
          validates :email,uniqueness: true
          validates :password, format: {with:/\A[a-zA-Z0-9]+\z/}
          validates :nickname,presence: true
-         validates :first_name_hurigana,presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
-         validates :first_name,presence: true, format: {with:/\A[一-龥]+\z/}
-         validates :last_name_hurigana,presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
-         validates :last_name,presence: true, format: {with:/\A[一-龥]+\z/}
-         validates :date,presence: true
+         
+         with_options format: {with:/\A[ぁ-んァ-ヶ一-龥]+\z/} do
+          validates :first_name,presence: true
+          validates :last_name,presence: true
+         end
+         
+         with_options format: {with:/\A[ァ-ヶー－]+\z/} do
+          validates :first_name_hurigana,presence: true
+          validates :last_name_hurigana,presence: true
+        end
+         
+        validates :date,presence: true
 
          has_many :items
          has_many :buys
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -76,8 +76,59 @@ RSpec.describe User, type: :model do
       @user.date = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Date can't be blank")
-    end  
-   end
+    end 
+    it 'emailは一意性出ないと登録できない'do
+      @user = FactoryBot.create(:user)
+      another_user = FactoryBot.build(:user,email: @user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end 
+    it 'emailは@がないと登録できない'do
+      @user.email = 'sample'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end 
+    it 'passwordは６文字以下では登録できない'do
+      @user.password = 's1111'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end 
+    it 'passwordは数字のみでは登録できない'do
+      @user.password = '111111'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end 
+    it 'passwordは英字のみでは登録できない'do
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end 
+    it 'passwordは全角英数字では登録できない'do
+      @user.password = 'A１１１１１'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end 
+    it 'first_nameが漢字・平仮名・カタカナ以外は登録できない'do
+      @user.first_name = 'yamada'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid")
+    end 
+    it 'first_name_huriganaがカタカナ以外では登録できない'do
+      @user.first_name_hurigana = '山田'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name hurigana is invalid")
+    end 
+    it 'last_nameが漢字・平仮名・カタカナ以外は登録できない'do
+      @user.last_name = 'tarou'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid")
+    end 
+    it 'last_name_huriganaがカタカナ以外は登録できない'do
+      @user.last_name_hurigana = '太朗'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name hurigana is invalid")
+    end 
+  end
  end
 end
 
