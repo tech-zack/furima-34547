2cb8af24904288fcbedabe8033be7468eb6e0815
tech-zack <difference19@icloud.com>
2021-02-21 21:46:51 +0900

    商品削除機能訂正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create,:edit,:update]
+  before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy]
   before_action :set_item, only:[:edit,:update,:show, :destroy]
   before_action :move_to_index, except: [:index, :show,:new,:create]
   def index
